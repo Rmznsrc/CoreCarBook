d164c002cd2300328235857d3416f48c109aa6b9
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-12-01 23:49:35 +0300

    Feature CQRS Processes with Mediatr - 2

---
+++ Core/CoreCarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CoreCarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CoreCarBook.Application.Interfaces;
+using CoreCarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
+    {
+        private readonly IRepository<Feature> _repository;
+
+        public CreateFeatureCommandHandler(IRepository<Feature> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Feature
+            {
+                 Name = request.Name, 
+            });
+        }
+    }
+}
---
+++ Core/CoreCarBook.Application/Features/Mediator/Handlers/FeatureHandlers/GetFeatureByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CoreCarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using CoreCarBook.Application.Features.Mediator.Results.FeatureResults;
+using CoreCarBook.Application.Interfaces;
+using CoreCarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class GetFeatureByIdQueryHandler : IRequestHandler<GetFeatureByIdQuery, GetFeatureByIdQueryResult>
+    {
+        private readonly IRepository<Feature> _repository;
+        public async Task<GetFeatureByIdQueryResult> Handle(GetFeatureByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetFeatureByIdQueryResult
+            {
+                FeatureID = values.FeatureID,
+                Name = values.Name
+            };
+        }
+    }
+}
