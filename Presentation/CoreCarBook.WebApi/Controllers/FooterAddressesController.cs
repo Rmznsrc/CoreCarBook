1751029d828e1d76877fba62ef074f0f503d4314
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-12-03 00:12:43 +0300

    .

---
+++ Presentation/CoreCarBook.WebApi/Controllers/FooterAddressesController.cs
@@ -0,0 +1,52 @@
+﻿using CoreCarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CoreCarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers;
+using CoreCarBook.Application.Features.Mediator.Queries.FooterAddressQueries;
+using CoreCarBook.Application.Features.Mediator.Queries.FooterAdressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FooterAddressesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressList()
+        {
+            var values = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Alt adres bilgisi eklendi.");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddress(int id)
+        {
+            var values = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+           await _mediator.Send(new RemoveFooterAddressCommand(id));
+           return Ok("Alt adres bilgisi silindi.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Alt adres bilgisi güncellendi.");
+        }
+    }
+}
