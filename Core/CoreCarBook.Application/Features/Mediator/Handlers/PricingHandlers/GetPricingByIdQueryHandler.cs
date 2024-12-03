4778c37e5965d3e43ff7b11166a6749590b333c3
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-12-04 01:53:12 +0300

    Pricing CQRS Mediatr Processes

---
+++ Core/CoreCarBook.Application/Features/Mediator/Handlers/PricingHandlers/GetPricingByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using CoreCarBook.Application.Features.Mediator.Queries.PricingQueries;
+using CoreCarBook.Application.Features.Mediator.Results.PricingResults;
+using CoreCarBook.Application.Interfaces;
+using CoreCarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCarBook.Application.Features.Mediator.Handlers.PricingHandlers
+{
+    public class GetPricingByIdQueryHandler : IRequestHandler<GetPricingByIdQuery, GetPricingByIdQueryResult>
+    {
+        private readonly IRepository<Pricing> _repository;
+
+        public GetPricingByIdQueryHandler(IRepository<Pricing> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetPricingByIdQueryResult> Handle(GetPricingByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            return new GetPricingByIdQueryResult
+            {
+                Name = value.Name,
+                PricingID = value.PricingID 
+            };
+        }
+    }
+}
