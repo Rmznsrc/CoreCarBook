b8aea2e2188badb18b4337658bded8af1e8330f2
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-12-12 00:47:19 +0300

    Contact Web API Post Processes

--- Frontends/CoreCarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CoreCarBook.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreCarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CoreCarBook.WebUI.Controllers
 {
@@ -10,9 +13,23 @@ namespace CoreCarBook.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7193/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
             return View();
         }
     }
