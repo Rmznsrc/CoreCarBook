5fb90d47dbe558e96e9dcba0a000cbd2b5322b6a
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-12-14 21:34:34 +0300

    Edited Home Page

---
+++ Frontends/CoreCarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCarBook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCoverUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
